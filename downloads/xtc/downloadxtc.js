a393dfaf7c8a5c21149fe0baf6bff62313c6ced6
GXBF <guixiabaifo2022@outlook.com>
2024-06-24 18:06:40 +0800

    是

--- downloads/xtc/downloadxtc.js
+++ downloads/xtc/downloadxtc.js
@@ -1,39 +1,226 @@
 
         // 模拟从服务器获取的文件列表数据
         const files = [
-            { name: '【解决非法降级和非法安装】xtcmodule.zip', url: '/download_files/【解决非法降级和非法安装】xtcmodule.zip', description: '本模块将开启debug，改版本号为9.9.9。' },
-            { name: '【微信小程序】com.tencent.weapp.wear.APK', url: '/download_files/【微信小程序】com.tencent.weapp.wear.APK', description: '暂无描述。' },
-            { name: '【我就有QFIL】QPST.rar', url: '/download_files/【我就有QFIL】QPST.rar', description: '暂无描述。' },
-            { name: '【需解压】CaremeOS可用的EDXP框架.zip', url: '/download_files/【需解压】CaremeOS可用的EDXP框架.zip', description: '暂无描述。' },
-            { name: '【需解压】skyui-1.9-Z7-release（务必先解压查看使用说明）.zip', url: '/download_files/【需解压】skyui-1.9-Z7-release（务必先解压查看使用说明）.zip', description: '暂无描述。' },
-            { name: '【需解压】强力监测-8.5.6(检测手表运行状态,强力推荐).zip', url: '/download_files/【需解压】强力监测-8.5.6(检测手表运行状态,强力推荐).zip', description: '暂无描述。' },
-            { name: 'adb移除所有Magisk模块.bat', url: '/download_files/adb移除所有Magisk模块.bat', description: '暂无描述。' },
-            { name: 'ANA OS2-1.6-Z7 200.zip', url: '/download_files/ANA OS2-1.6-Z7 200.zip', description: '暂无描述。' },
-            { name: 'ANA OS附属模块2-Z7.zip', url: '/download_files/ANA OS附属模块2-Z7.zip', description: '暂无描述。' },
-            { name: 'CaremeOSPro_1.1_release.zip', url: '/download_files/CaremeOSPro_1.1_release.zip', description: '暂无描述。' },
-            { name: 'fdac22ba-delta修改自动响应.APK', url: '/download_files/fdac22ba-delta修改自动响应.APK', description: '暂无描述。' },
-            { name: 'HarmonyOS Sans字体模块 .zip', url: '/download_files/HarmonyOS Sans字体模块 .zip', description: '暂无描述。' },
-            { name: 'Magisk Delta 25210_f470988f.Apk', url: '/download_files/Magisk Delta 25210_f470988f.Apk', description: '暂无描述。' },
-            { name: 'Magisk卡刷包(低版本当模块刷).zip', url: '/download_files/Magisk卡刷包(低版本当模块刷).zip', description: '暂无描述。' },
-            { name: 'prog_emmc_firehose_8937_ddr_hisen.mbn', url: '/download_files/prog_emmc_firehose_8937_ddr_hisen.mbn', description: '暂无描述。' },
-            //{ name: '', url: '/download_files/', description: '暂无描述。' },
-        ];
+            {
+            "name": "【微信小程序】com.tencent.weapp.wear.APK",
+            "url": "/download_files/【微信小程序】com.tencent.weapp.wear.APK",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "【我就有QFIL】QPST.rar",
+            "url": "/download_files/【我就有QFIL】QPST.rar",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "【需解压】CaremeOS可用的EDXP框架.zip",
+            "url": "/download_files/【需解压】CaremeOS可用的EDXP框架.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "【需解压】skyui-1.9-Z7-release（务必先解压查看使用说明）.zip",
+            "url": "/download_files/【需解压】skyui-1.9-Z7-release（务必先解压查看使用说明）.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "【需解压】强力监测-8.5.6(检测手表运行状态，强力推荐).zip",
+            "url": "/download_files/【需解压】强力监测-8.5.6(检测手表运行状态，强力推荐).zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "adb移除所有Magisk模块.bat",
+            "url": "/download_files/adb移除所有Magisk模块.bat",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "ANA OS2-1.6-Z7 200.zip",
+            "url": "/download_files/ANA OS2-1.6-Z7 200.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "ANA OS附属模块2-Z7.zip",
+            "url": "/download_files/ANA OS附属模块2-Z7.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "CaremeOSPro_1.1_release.zip",
+            "url": "/download_files/CaremeOSPro_1.1_release.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "fdac22ba-delta修改自动响应.APK",
+            "url": "/download_files/fdac22ba-delta修改自动响应.APK",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "HarmonyOS Sans字体模块 .zip",
+            "url": "/download_files/HarmonyOS Sans字体模块 .zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Magisk Delta 25210_f470988f.Apk",
+            "url": "/download_files/Magisk Delta 25210_f470988f.Apk",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Magisk卡刷包(低版本当模块刷).zip",
+            "url": "/download_files/Magisk卡刷包(低版本当模块刷).zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "prog_emmc_firehose_8937_ddr_hisen.mbn",
+            "url": "/download_files/prog_emmc_firehose_8937_ddr_hisen.mbn",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "QPST_2.7.460.zip",
+            "url": "/download_files/QPST_2.7.460.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Z7（I25）产品介绍 20210701 (1).pptx",
+            "url": "/download_files/Z7（I25）产品介绍 20210701 (1).pptx",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Z7（I25）产品介绍 20210701.pptx",
+            "url": "/download_files/Z7（I25）产品介绍 20210701.pptx",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Z8_TWRP_274-280.img",
+            "url": "/download_files/Z8_TWRP_274-280.img",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Z8A_V1.2.0_magisk25.2_sboot.img",
+            "url": "/download_files/Z8A_V1.2.0_magisk25.2_sboot.img",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "Z9_V2.5.2_magisk25.2_sboot.img",
+            "url": "/download_files/Z9_V2.5.2_magisk25.2_sboot.img",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "安装magisk模块 ver 2.3 (1).bat",
+            "url": "/download_files/安装magisk模块 ver 2.3 (1).bat",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "安装magisk模块 ver 2.3.bat",
+            "url": "/download_files/安装magisk模块 ver 2.3.bat",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "倒卖文件公示.docx",
+            "url": "/download_files/倒卖文件公示.docx",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "电池健康度_Magisk.zip",
+            "url": "/download_files/电池健康度_Magisk.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "关于ANA OS-1.2.Apk",
+            "url": "/download_files/关于ANA OS-1.2.Apk",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "画板 1 (1).png",
+            "url": "/download_files/画板 1 (1).png",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "画板 1.png",
+            "url": "/download_files/画板 1.png",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "画板 2 (1).png",
+            "url": "/download_files/画板 2 (1).png",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "画板 2.png",
+            "url": "/download_files/画板 2.png",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "解决输入法无法开机自启 (1).txt",
+            "url": "/download_files/解决输入法无法开机自启 (1).txt",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "解决输入法无法开机自启.txt",
+            "url": "/download_files/解决输入法无法开机自启.txt",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "手表必要信息备份-24.5.29.bat",
+            "url": "/download_files/手表必要信息备份-24.5.29.bat",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "微聊_5.3.20.Develop_AW_Weichat_J391_Ge4bef61_Dbb55a2a9.Apk",
+            "url": "/download_files/微聊_5.3.20.Develop_AW_Weichat_J391_Ge4bef61_Dbb55a2a9.Apk",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "微聊新创极光5.3.65修改版.Apk",
+            "url": "/download_files/微聊新创极光5.3.65修改版.Apk",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "向设备安装HttpCanary(黄&蓝鸟)根证书.zip",
+            "url": "/download_files/向设备安装HttpCanary(黄&蓝鸟)根证书.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "骁龙开核超频2.apk",
+            "url": "/download_files/骁龙开核超频2.apk",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "小天才破解史.docx",
+            "url": "/download_files/小天才破解史.docx",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "原神开机动画.zip",
+            "url": "/download_files/原神开机动画.zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "真实发光图标(来自小米主题).zip",
+            "url": "/download_files/真实发光图标(来自小米主题).zip",
+            "description": "暂无描述。"
+            },
+            {
+            "name": "自检-24.207.01111523.Apk",
+            "url": "/download_files/自检-24.207.01111523.Apk",
+            "description": "暂无描述。"
+            }
+            ];
+        
 
-        // 获取文件列表的DOM元素
-        const fileList = document.getElementById('file-list');
-        const fileDescription = document.getElementById('file-description');
+      // 获取文件列表的DOM元素
+const fileList = document.getElementById('file-list');
+const fileDescription = document.getElementById('file-description');
 
-        // 遍历文件列表数据，创建并添加列表项
-        files.forEach(file => {
-            const listItem = document.createElement('li');
-            listItem.textContent = file.name;
-            listItem.addEventListener('click', () => {
-                fileDescription.textContent = file.description;
-                const downloadButton = document.createElement('a');
-                downloadButton.href = file.url;
-                downloadButton.textContent = '下载';
-                downloadButton.style.marginLeft = '1rem';
-                fileDescription.appendChild(downloadButton);
-            });
-            fileList.appendChild(listItem);
-        });
+// 遍历文件列表数据，创建并添加列表项
+files.forEach(file => {
+    const listItem = document.createElement('li');
+    listItem.textContent = file.name;
+    listItem.addEventListener('click', () => {
+        // fileDescription.textContent = file.description;
+        const downloadButton = document.createElement('a');
+        downloadButton.href = file.url;
+        downloadButton.textContent = '下载';
+        downloadButton.style.marginLeft = '1rem';
+        fileDescription.appendChild(downloadButton);
+        downloadButton.click(); // 点击下载按钮时触发下载
+    });
+    fileList.appendChild(listItem);
+});
+
